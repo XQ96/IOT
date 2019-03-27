e8b0c0faebb59c9f39ce42e178bd9f3bcf5407d4
mf1832199@smail.nju.edu.cn <mf1832199@smail.nju.edu.cn>
2019-03-27 14:06:01 +0800

    init

---
+++ mqtt/mqtt_re.py
@@ -0,0 +1,29 @@
+﻿import paho.mqtt.client as mqtt
+import time
+
+HOST = "47.100.240.126"
+PORT = 1883
+
+def client_loop():
+    client_id = time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
+    client = mqtt.Client(client_id)    # ClientId不能重复，所以使用当前时间
+    client.username_pw_set("xuqi", "qwert")  # 必须设置，否则会返回「Connected with result code 4」
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(HOST, PORT, 60)
+    client.loop_forever()
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("test3")
+
+def on_message(client, userdata, msg):
+    data = msg.payload.decode("utf-8")
+    print(msg.topic+" "+data)
+    with open('out_mqtt.txt','a',encoding='utf-8') as f:
+        f.write(data+'\n')
+
+
+if __name__ == '__main__':
+    print('start....')
+    client_loop()
---
+++ socket/server.py
@@ -0,0 +1,47 @@
+﻿# 导入 socket、sys 模块
+import socket
+import sys
+from _thread import *
+
+# 创建 socket 对象
+serversocket = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM) 
+
+# 获取本地主机名
+host ="172.19.128.98"
+port = 9999
+
+# 绑定端口号
+serversocket.bind((host, port))
+
+# 设置最大连接数，超过后排队
+serversocket.listen(10)
+    # 建立客户端连接
+clientsocket,addr = serversocket.accept()    
+  # print (msg.decode('utf-8'))
+
+def clientthread(clientsocket):
+    while True:
+        try:
+            data = clientsocket.recv(1024)
+            if data == b'' or data == b' ':
+            	break
+            if data is not None or data != '':
+                msg = data.decode('utf-8')
+                print(msg)
+                with open('out_socket.txt','a',encoding='utf-8') as f:
+                    f.write(msg+'\n')
+
+            else: print('None')
+            # data_loaded = json.loads(data)
+        except socket.error:
+                print("One Client Connected over!")
+                break
+    clientsocket.close()
+
+    # msg='欢迎访问菜鸟教程！'+ "\r\n"
+    # clientsocket.send(msg.encode('utf-8'))
+clientthread(clientsocket)
+
+# clientsocket.close()
+serversocket.close()
